0891422a6f2d94081e1be5a669ba8d666aa90527
leahpd <44067914+leahpd@users.noreply.github.com>
2018-12-17 11:18:27 -0500

    Add files via upload

---
+++ problem2.py
@@ -0,0 +1,12 @@
+print('_'*60)
+print('Locker Bot:')
+print()
+password = input('What planet are you from?')
+if password == 'Mars':
+	print('Password is valid!')
+	print('Welcome to Earth, please do not kill us!')
+else:
+	print('Dammmmnnnnn, your face is so ugly I thought you were an alien. Anyway... have a good day!')
+	print('Pew Pew Pew')
+	print('Pew Pew Pew')
+print ('_'*60)
